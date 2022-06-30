6dd7397306582d43d1a5ea26cdab292c437d793b
Amit <amit.kumar@demandfarm.com>
2022-06-30 10:35:09 +0530

    Removed dummy injected text added to debug

--- inject.js
+++ inject.js
@@ -635,15 +635,6 @@ html #console-panel .dev-tools-resize-handle {
 `;
 
 (function() {
-
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
-
 	const style = document.createElement('style');
 	style.textContent = css;
 	document.head.append(style);
