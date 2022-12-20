1152b792902e8bcdd4bb5f01ebde0a2a7565ef6c
Amit Kumar <amitcute3@gmail.com>
2022-12-20 11:53:42 +0530

    Update console-panel.js

--- console-panel.js
+++ console-panel.js
@@ -6,6 +6,28 @@ function loadScript(name) {
 }
 
 (function() {
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		const button = document.getElementById("toggle-console-panel-btn");
+
+		if (request.action === 'DEACTIVATE_APP') {			
+			if (button) {
+				button.setAttribute("data", request.tabId); 
+				button.setAttribute("data-status", request.action); 
+				button.click(request.tabId);
+			}
+		}
+		
+		if (request.action === 'ACTIVATE_APP') {
+			if (button) {
+				button.setAttribute("data", request.tabId); 
+				button.setAttribute("data-status", request.action); 
+				button.click(request.tabId);
+			}
+		}
+
+		return sendResponse('pong');
+	});
+	
 	loadScript('network.js');
 	loadScript('jsoneditor.min.js');
 	loadScript('panel.js');
